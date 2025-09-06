c861efa9849a7f93a90aeeabbecc76bb01c3ffda
Maana <118558385+Ammandev@users.noreply.github.com>
2025-09-07 00:30:51 +0500

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/NFT-Air-Hockey/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
